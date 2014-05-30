833cd5cd2e4322568bd6c18620a1bdef8a666d17
kuaikanyouyueliang <1534023616@qq.com>
2014-05-30 10:07:41 +0800

    lion

--- src/commons/com/lion/common/PropertiesUtilSecond.java
+++ src/commons/com/lion/common/PropertiesUtilSecond.java
@@ -3,11 +3,6 @@ package com.lion.common;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
-
-/**
- *解析properties文件 试用inputstream 
- *要指定lion.properties文件的路径;
- */
 public class PropertiesUtilSecond {
 	public static void main(String[] args) {
 		try {
