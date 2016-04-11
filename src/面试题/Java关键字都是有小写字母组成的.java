4f6562fc8977edbd45d098e9c67871363b98117f
kuaikanyouyueliang <1534023616@qq.com>
2016-04-11 21:56:16 +0800

    learn

--- src/面试题/Java关键字都是有小写字母组成的.java
+++ src/面试题/Java关键字都是有小写字母组成的.java
@@ -3,5 +3,10 @@
  * Date:2016/4/11 21:14
  */
 public class Java关键字都是有小写字母组成的 {
-
+    public static void main(String[] args) {
+//        System.out.println(args.length);
+        for (String str:args){
+            System.out.println(str);
+        }
+    }
 }
