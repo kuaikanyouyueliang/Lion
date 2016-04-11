8ff638f11036762e8a4ddaf00d84e5d05d989c8d
kuaikanyouyueliang <1534023616@qq.com>
2016-04-11 22:08:05 +0800

    learn

---
+++ src/面试题/equals和等号的区别.java
@@ -0,0 +1,13 @@
+/**
+ * User:Administrator
+ * Date:2016/4/11 21:57
+ */
+public class equals和等号的区别 {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = new Integer(1);
+        System.out.println(a == b);//针对引用类型比较的是变量所指向的内存地址
+        System.out.println(a.equals(b));//Integer重写了equals方法，比较的是内容
+        System.out.println(a==b.intValue());
+    }
+}
