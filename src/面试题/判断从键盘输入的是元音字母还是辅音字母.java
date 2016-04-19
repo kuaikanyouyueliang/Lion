0b9545a4e9379367a386acf6e33881245e166053
kuaikanyouyueliang <1534023616@qq.com>
2016-04-19 22:06:56 +0800

    learn

--- src/面试题/判断从键盘输入的是元音字母还是辅音字母.java
+++ src/面试题/判断从键盘输入的是元音字母还是辅音字母.java
@@ -18,7 +18,7 @@ public class 判断从键盘输入的是元音字母还是辅音字母 {
                 case 'i':
                 case 'o':
                 case 'u':
-                    System.out.println(c+":是一个元音字母1");
+                    System.out.println(c+":是一个元音字母");
                     break;
                 default:
                     System.out.println(c+":是一个辅音字母");
